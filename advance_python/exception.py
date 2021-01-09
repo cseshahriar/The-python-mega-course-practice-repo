641c30dbd3e463b9a355463dd8c0872bf5cfae74
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-09 23:26:30 +0600

    exception handle

--- advance_python/exception.py
+++ advance_python/exception.py
@@ -20,12 +20,22 @@ class ValueTooHighError(Exception):
     pass
 
 
+class ValueTooSmallError(Exception):
+    def __init__(self, message, value):
+        self.message = message
+        self.value = value
+
+
 def test_value(x):
     if x > 100:
         raise ValueTooHighError('Value is to high')
+    if x < 5:
+        raise ValueTooSmallError('Value is to small', x)
 
 
 try:
-    test_value(200)
+    test_value(4)
 except ValueTooHighError as e:
     print(e)
+except ValueTooSmallError as e:
+    print(e.message, e.value)
